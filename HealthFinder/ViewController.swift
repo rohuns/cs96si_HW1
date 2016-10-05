4b5c9e4acb36e35bbe44a02079a4077aae1347b6
Rohun Saxena <rohuns@stanford.edu>
2016-10-05 15:21:03 -0700

    Initial Commit

---
+++ HealthFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthFinder
+//
+//  Created by Rohun Saxena on 10/5/16.
+//  Copyright © 2016 Rohun Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
